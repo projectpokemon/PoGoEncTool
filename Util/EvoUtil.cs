50b25d396dba47ae4d946f50a9743dd59ec3fcc0
Kurt <kaphotics@gmail.com>
2020-11-14 19:51:23 -0800

    Get for g7+g8

--- Util/EvoUtil.cs
+++ Util/EvoUtil.cs
@@ -9,8 +9,16 @@ namespace PoGoEncTool
     {
         public static IEnumerable<int> Get(int species, int form)
         {
-            var method = typeof(EvolutionTree).GetMethods(BindingFlags.Static | BindingFlags.NonPublic).First(z => z.Name == "GetEvolutionTree" && z.GetParameters().Length == 1);
-            var tree = (EvolutionTree)method.Invoke(null, new object[] {8})!;
+            var g8 = Get(species, form, 8);
+            var g7 = Get(species, form, 7);
+            return g8.Concat(g7).Distinct();
+        }
+
+        private static IEnumerable<int> Get(int species, int form, int gen)
+        {
+            var method = typeof(EvolutionTree).GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
+                .First(z => z.Name == "GetEvolutionTree" && z.GetParameters().Length == 1);
+            var tree = (EvolutionTree) method.Invoke(null, new object[] {gen})!;
 
             var evoMethod = typeof(EvolutionTree).GetMethod("GetEvolutions", BindingFlags.Instance | BindingFlags.NonPublic)!;
             var result = evoMethod.Invoke(tree, new object[] {species, form})!;
