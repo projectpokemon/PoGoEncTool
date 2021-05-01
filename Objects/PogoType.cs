7c2575d8a893d4ea8c68a8a82560e2cf2d7d5533
sora10pls <17801814+sora10pls@users.noreply.github.com>
2021-04-30 21:06:00 -0400

    Add PogoType for Strange Eggs
    So this was an oversight. Because Strange Eggs can only be obtained by defeating Team GO Rocket Leaders, and you can only battle Team GO Rocket starting from Lv. 8, it's impossible to have something from a Strange Egg hatch at Lv. 1 through 7.
    
    Affected species are Sandile, Scraggy, Pawniard, Vullaby, and the upcoming Pancham.
    
    Co-Authored-By: Lusamine <30205550+Lusamine@users.noreply.github.com>

--- Objects/PogoType.cs
+++ Objects/PogoType.cs
@@ -4,8 +4,13 @@
     {
         None, // Don't use this.
 
+        /// <summary> Wild encounter, no special requirements </summary>
         Wild,
+
+        /// <summary> Pokémon Egg, requires Lv. 1 and IV = 1 </summary>
         Egg,
+        /// <summary> Strange Egg, requires Lv. 8 and IV = 1 </summary>
+        EggS,
 
         /// <summary> Raid Boss, requires Lv. 20 and IV = 1 </summary>
         Raid = 10,
