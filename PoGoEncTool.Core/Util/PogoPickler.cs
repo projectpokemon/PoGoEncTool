7062bdca21996634684f5bc18761f97431d864b9
Kurt <kaphotics@gmail.com>
2022-05-19 16:27:27 -0700

    Calculate group on export
    Old logic would defer that determination to PKHeX. We can do that ourselves now.

--- PoGoEncTool.Core/Util/PogoPickler.cs
+++ PoGoEncTool.Core/Util/PogoPickler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using PKHeX.Core;
 using static PKHeX.Core.Species;
 
 namespace PoGoEncTool.Core
@@ -30,8 +31,9 @@ namespace PoGoEncTool.Core
             using var ms = new MemoryStream();
             using var bw = new BinaryWriter(ms);
 
-            var sf = entry.Species | (entry.Form << 11);
-            bw.Write((ushort)sf);
+            bw.Write((ushort)entry.Species);
+            bw.Write((byte)entry.Form);
+            bw.Write((byte)GetGroup(entry.Species, entry.Form));
             foreach (var a in entry.Data)
                 Write(a, bw);
 
@@ -46,6 +48,52 @@ namespace PoGoEncTool.Core
             byte sg = (byte) (PogoToHex(entry.Shiny) | (PogoToHex(entry.Gender) << 6));
             bw.Write(sg);
             bw.Write((byte)entry.Type);
+        }
+
+        public enum PogoImportFormat : byte
+        {
+            PK7 = 0,
+            PB7 = 1,
+            PK8 = 2,
+            PA8 = 3,
+        }
+
+        private static PogoImportFormat GetGroup(int species, int form)
+        {
+            // Transfer Rules:
+            // If it can exist in LGP/E, it uses LGP/E's move data for the initial import.
+            // Else, if it can exist in SW/SH, it uses SW/SH's move data for the initial import.
+            // Else, if it can exist in PLA, it uses PLA move data for the initial import.
+            // Else, it must exist in US/UM, thus it uses US/UM for the initial import.
+
+            if (species is (<= 151 or 808 or 809))
+            {
+                if (form == 0 || PersonalTable.GG[species].HasForm(form))
+                    return PogoImportFormat.PB7;
+            }
+
+            if (species <= 898) // Legal.MaxSpeciesID_8
+            {
+                if (((PersonalInfoSWSH)PersonalTable.SWSH.GetFormEntry(species, form)).IsPresentInGame)
+                    return PogoImportFormat.PK8;
+            }
+
+            if (species <= 807) // Legal.MaxSpeciesID_7_USUM
+            {
+                if (form == 0 || PersonalTable.USUM[species].HasForm(form))
+                    return PogoImportFormat.PK7;
+            }
+
+            if (species <= 905) // Legal.MaxSpeciesID_8a
+            {
+                if (((PersonalInfoLA)PersonalTable.LA.GetFormEntry(species, form)).IsPresentInGame)
+                    return PogoImportFormat.PA8;
+            }
+
+            if (species <= 807) // Legal.MaxSpeciesID_7_USUM
+                return PogoImportFormat.PK7;
+
+            throw new ArgumentOutOfRangeException(nameof(species));
         }
 
         private static byte PogoToHex(PogoShiny type) => type switch
