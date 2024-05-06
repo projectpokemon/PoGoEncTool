1ec0f91e08485e37d82cab4b9a0da0a21fbdee07
sora10pls <17801814+sora10pls@users.noreply.github.com>
2024-05-06 13:20:34 -0400

    Add handling for Wonder Ticket Poipole
    No HUD, Poké/Great/Ultra are permitted, but Master Ball can not be used
    Has a more permissive IV restriction compared to Mythicals, so, need to separate the two
    Adjust comment for Mewtwo raid enc

--- PoGoEncTool.Core/Objects/PogoType.cs
+++ PoGoEncTool.Core/Objects/PogoType.cs
@@ -1,4 +1,4 @@
-﻿namespace PoGoEncTool.Core;
+namespace PoGoEncTool.Core;
 
 public enum PogoType : byte
 {
@@ -27,32 +27,40 @@ public enum PogoType : byte
     /// <summary> Mythical Pokémon captured after completing Field Research. </summary>
     ResearchM,
     /// <summary> Mythical Pokémon captured after completing Field Research. Only Poké Balls can be used. </summary>
-    ResearchP,
+    ResearchMP,
     /// <summary> Ultra Beasts captured after completing Field Research. Only Beast Balls can be used. </summary>
     ResearchUB,
     /// <summary> Mythical Pokémon captured after completing Field Research. No HUD is visible during these encounters. </summary>
-    /// <remarks> Under normal circumstances, only Poké Balls can be used, but Great Balls and Ultra Balls can be used with the Remember Last-Used Poké Ball setting. </remarks>
+    /// <remarks>
+    /// Under normal circumstances, only Poké Balls can be used, but Great Balls and Ultra Balls can be used with the Remember Last-Used Poké Ball setting.
+    /// This was rendered unusable as of version 0.277.3.
+    /// </remarks>
+    ResearchMH,
+    /// <summary> Pokémon captured after completing Field Research. No HUD is visible during these encounters. </summary>
+    /// <remarks>
+    /// The encounter defaults to the player's stock of Poké Balls. If they have none, it falls back to Great Balls, and then to Ultra Balls.
+    /// If the player has no Poké Balls, Great Balls, or Ultra Balls, the HUD fails to load in any Poké Ball at all, even if they have a Master Ball.
+    /// </remarks>
     ResearchNH,
 
     /// <summary> Pokémon captured from the GO Battle League. </summary>
     GBL = 30,
     /// <summary> Mythical Pokémon captured from the GO Battle League. </summary>
     GBLM,
-    /// <summary> Pokémon captured from the GO Battle League during GO Battle Day, excluding Legendary and Mythical Pokémon. </summary>
+    /// <summary> Pokémon captured from the GO Battle League during GO Battle Day events. Excludes Legendary Pokémon, Mythical Pokémon, and Ultra Beasts. </summary>
     GBLD,
 
     /// <summary> Pokémon captured after defeating members of Team GO Rocket. Must be Purified before transferring to Pokémon HOME. </summary>
     /// <remarks> Pokémon with this <see cref="PogoType"/> can not be moved to <see cref="PKHeX.Core.GameVersion.GG"/>. </remarks>
     Shadow = 40,
 
-    /// <summary>
-    /// Pokémon captured from Special Research or Timed Research with a Premier Ball.
-    /// </summary>
+    /// <summary> Pokémon captured from Special Research or Timed Research with a Premier Ball. </summary>
     /// <remarks>
-    /// Niantic pushed release 0.269 on April 22, 2023, which contained an issue with the Remember Last-Used Poké Ball setting.
+    /// Niantic released version 0.269.0 on April 22, 2023, which contained an issue with the Remember Last-Used Poké Ball setting.
     /// This allowed for Premier Balls obtained from Raid Battles to be remembered on all future encounters.
-    /// The moment the Premier Ball touched the floor or a wild Pokémon, the encounter would end, except if it was from a Special Research and Timed Research encounter.
+    /// The moment the Premier Ball touched the floor or a wild Pokémon, the encounter would end, except if it was from a Special Research, Timed Research, or Collection Challenge encounter.
     /// This made it possible for over 300 species of Pokémon to be obtainable in a Poké Ball they were never meant to be captured in.
+    /// This bug was fixed with the release of version 0.269.2.
     /// </remarks>
     Research269 = 200,
     Research269M,
