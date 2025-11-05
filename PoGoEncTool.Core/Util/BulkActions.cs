d1d1eac12bb4065f0d3f27ef909bf200f7bedf1b
sora10pls <17801814+sora10pls@users.noreply.github.com>
2025-11-05 16:28:51 -0500

    Dynamax Eevee Max Battle Weekend

--- PoGoEncTool.Core/Util/BulkActions.cs
+++ PoGoEncTool.Core/Util/BulkActions.cs
@@ -229,7 +229,7 @@ public static class BulkActions
         Gastly => 1,
         Krabby => 1,
         Chansey => 3,
-        Eevee => 1, // verify
+        Eevee => 2,
         Omanyte => 1,
         Kabuto => 1,
         Shuckle => 2,
