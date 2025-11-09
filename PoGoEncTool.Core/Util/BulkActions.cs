60e770e6a2e0e8d9747a6895f37634680e94ea30
sora10pls <17801814+sora10pls@users.noreply.github.com>
2025-11-09 11:07:35 -0500

    Update BulkActions.cs

--- PoGoEncTool.Core/Util/BulkActions.cs
+++ PoGoEncTool.Core/Util/BulkActions.cs
@@ -233,7 +233,7 @@ public static class BulkActions
         Omanyte => 1,
         Kabuto => 1,
         Shuckle => 2,
-        Ralts => 1, // verify
+        Ralts => 1,
         Sableye => 3,
         Wailmer => 2,
         Beldum => 3,
@@ -243,7 +243,7 @@ public static class BulkActions
         Darumaka => 2,
         Trubbish => 1,
         Cryogonal => 3,
-        Inkay => 1, // verify
+        Inkay => 1,
         Bounsweet => 1,
         Passimian => 3,
         Grookey => 1,
