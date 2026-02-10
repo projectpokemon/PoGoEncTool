1c16e0d60968dbae41c1a2d5ec9cff0130240697
sora10pls <17801814+sora10pls@users.noreply.github.com>
2026-02-10 07:47:32 -0500

    Raid Bosses: February 10-15, 2026

--- PoGoEncTool.Core/Util/BulkActions.cs
+++ PoGoEncTool.Core/Util/BulkActions.cs
@@ -311,7 +311,7 @@ public static class BulkActions
         Charmander => 1,
         Squirtle => 1,
         Caterpie => 1,
-        Growlithe => 1, // verify
+        Growlithe => 1,
         Abra => 1,
         Machop => 2,
         Gastly => 1,
