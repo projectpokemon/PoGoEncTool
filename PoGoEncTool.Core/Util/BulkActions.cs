b6ab9494e277b5e8f7fa9277951c23a52693624e
sora10pls <17801814+sora10pls@users.noreply.github.com>
2026-09-15 19:38:57 -0400

    Raid Bosses: September 16, 2026

--- PoGoEncTool.Core/Util/BulkActions.cs
+++ PoGoEncTool.Core/Util/BulkActions.cs
@@ -248,7 +248,7 @@ public static class BulkActions
             Krabby => 1,
             Hitmonlee => 3,
             Hitmonchan => 3,
-            Rhyhorn => 2, // verify
+            Rhyhorn => 1,
             Chansey => 3,
             Electabuzz => 3,
             Magmar => 3,
